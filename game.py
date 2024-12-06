e561f9d89ed17837cffba35c071df80e44f70eae
Kauan <144381805+KauanAfonso@users.noreply.github.com>
2024-12-06 15:49:41 -0300

    Adding new updates

--- game.py
+++ game.py
@@ -7,6 +7,7 @@ class Cards:
 
         self.list_cards_to_player = ["ACE" , "2", "3" ,"4" , "5", "6", "7", "8" , "9" , "10", "Jack", "Queen", "King"]
         self.list_categories = ["Hearts" , "Diamonts", "Clubs" , "Spodes"]
+        self.value =0
 
     def draw_cart(self):
         '''
@@ -32,14 +33,14 @@ class Cards:
 
     def check_card(self):
         '''
-        This function is resposible to chek each value tha was drawn and retorned it
+        This function is resposible to check each value tha was drawn and retorned it
         
         '''
         card = self.draw_cart()
 
         if card == "Jack" or card == "Queen" or card == "King":
             return 10
-        elif card == "Ace":
+        elif card == "ACE":
             return 11
         else:
             return int(card)
@@ -57,19 +58,46 @@ class Cards:
         This function makes a card`s hand of player and return this
         
         '''
-        card_drawn = self.check_card()
-        categories_drawn = self.draw_categories()
 
         players_hand = [
-            ["Cards: "], ["Categories"]
+            ["Cards: "], 
+            ["Categories:"]
         ]
 
-    
+        accumulate = 0
 
-        players_hand[0].append(card_drawn)
-        players_hand[1].append(categories_drawn)
+        while True:
 
-        return players_hand
+            '''
+            
+            Here, we are using our functions ago , and it doing the hand's value
+            
+            '''
+            
+
+            card_drawn = self.check_card()
+            categories_drawn = self.draw_categories()
+            
+
+            response = input("Do you wanna buy a card?  Answer with y or n:").upper()
+
+            if response == "Y" or response == "YES":
+
+                players_hand[0].append(card_drawn)
+                players_hand[1].append(categories_drawn)
+                accumulate += card_drawn
+                
+                print(players_hand)
+                print(accumulate)
+
+
+                if accumulate > 21:
+                    return accumulate
+                    
+            else:
+                break
+
+        return accumulate
     
         
 
@@ -91,25 +119,37 @@ class Game:
         return self.player.get_name()
     
     def response_player(self):
-        while True:
-            response = input("Do you wanna buy a card?  Answer with Y or Yes").upper()
-            if response == "Y" or response == "YES":
-                card = self.hand_cards_player.cards_played()
-            print(card)
+       
+        '''
+        This function will returned the hand's value 
+        after the player have choesen to stop
 
-
-teste = Game("Kauan")
-teste.response_player()
-
-
+        '''
+        
+        value = self.hand_cards_player.cards_played()            
+        return value
     
 
+player = Game(input("Put your name here: "))
+player2 = Game(input("Put your name here 2: "))
+
+name1 = player.get_name()
+name2 = player2.get_name()
+
+print(f"Turn to {name1} play: ")
+value1 = player.response_player()
+
+print(f"Turn to {name2} play")
+value2 = player2.response_player()
+
     
-    
-    
+def find_winner(name1, name2, value1, value2):
 
+    if value1 <=21 and value1 > value2:
+        print(f"{name1} won!")
+    elif value1 == value2:
+        print(f"Draw")
+    else:
+        print(f"{name2} won!")
 
-
-
-teste = Game(input('Digite seu nome: '))
-print(teste.get_name())
+find_winner(name1,name2,value1,value2)
